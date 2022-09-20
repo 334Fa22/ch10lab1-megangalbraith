1d45575f4de52c0370c8eeec4baa975c514da787
megangalbraith <s534205@nwmissouri.edu>
2022-09-20 13:04:51 -0500

    completed ch10 lab

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/*Megan Galbraith
+script.js
+September 20, 2022 */
+
+//Hamburger menu function
+function hamburger() {
+  var menu = document.getElementById ("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
